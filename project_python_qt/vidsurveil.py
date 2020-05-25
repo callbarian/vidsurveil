5ed5a0d2261cd179193856bb05aeb0c7ca509d17
callbarian <callbarian@github.com>
2020-05-26 00:28:34 +0900

    seongmin

--- project_python_qt/vidsurveil.py
+++ project_python_qt/vidsurveil.py
@@ -96,17 +96,19 @@ class vidsurveil(QDialog):
         print(out[0])
 
     def extract_features(self):
-        print("extracting........")
-        os.system('sh ' + '/home/callbarian/C3D/C3D-v1.0/examples/c3d_feature_extraction/feature_extraction.sh')
+        #print("extracting........")
+        #os.system('sh ' + '/home/callbarian/C3D/C3D-v1.0/examples/c3d_feature_extraction/feature_extraction.sh')
         #result = subprocess.Popen(['sh','/home/callbarian/C3D/C3D-v1.0/examples/c3d_feature_extraction/call_environment.sh','/home/callbarian/bin/miniconda3/envs/c3d_py36/bin/python','/home/callbarian/C3D/C3D-v1.0/examples/c3d_feature_extraction/extract_C3D_feature.py'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         #(out, err) = result.communicate()
         #print(out)
         #result.wait()
-
-        #result = subprocess.Popen(['sh','/home/callbarian/C3D/C3D-v1.0/examples/c3d_feature_extraction/call_environment.sh','/home/callbarian/bin/miniconda3/envs/Anomaly_py36/bin/python','/home/callbarian/AnomalyDetectionCVPR2018-master/Demo_GUI.py'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        #(out,err) = result.communicate()
-        #print(out)
-        #
+        
+        print("extracting timestamps and videos..........")
+        
+        result = subprocess.Popen(['sh','/home/callbarian/C3D/C3D-v1.0/examples/c3d_feature_extraction/call_environment.sh','/home/callbarian/bin/miniconda3/envs/Anomaly_py36/bin/python','/home/callbarian/AnomalyDetectionCVPR2018-master/Demo_GUI.py'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        (out,err) = result.communicate()
+        print(out)
+        
 
 if __name__ == "__main__":
     app = QApplication([])
